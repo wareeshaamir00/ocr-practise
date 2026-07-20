23665d03619398bf6627f8be94a578c85f28d20c
Wareesha Amir <amirwareesha00@gmail.com>
2026-07-20 15:02:02 +0500

    main program

--- main/main4.py
+++ main/main4.py
@@ -8,3 +8,6 @@ text = pytesseract.image_to_string(image)
 print(text)
 with open("output/text.txt", "w", encoding="utf-8") as f:
     f.write(text)
+with open ("output/text.txt", "r", encoding="utf-8") as f:
+    content = f.read()
+    print(content)
